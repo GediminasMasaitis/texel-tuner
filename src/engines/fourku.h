47c5366489aa26b79a26e001c806f88678cc04e9
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-11-06 00:45:19 +0200

    Fix 4ku config

--- src/engines/fourku.h
+++ src/engines/fourku.h
@@ -12,7 +12,7 @@ namespace Fourku
     {
     public:
         constexpr static bool includes_additional_score = true;
-        constexpr static bool supports_external_chess_eval = false;
+        constexpr static bool supports_external_chess_eval = true;
 
         static parameters_t get_initial_parameters();
         static EvalResult get_fen_eval_result(const std::string& fen);
