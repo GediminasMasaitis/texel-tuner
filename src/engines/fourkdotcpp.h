a85ee2571aeb2d3924c9e410ef2ee06c60181c0c
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2026-07-13 11:25:32 +0300

    longer

--- src/engines/fourkdotcpp.h
+++ src/engines/fourkdotcpp.h
@@ -18,11 +18,11 @@ namespace Fourkdotcpp
         constexpr static bool supports_external_chess_eval = true;
         constexpr static bool retune_from_zero = true;
         constexpr static tune_t preferred_k = 2.7;
-        constexpr static int32_t max_epoch = 5001;
+        constexpr static int32_t max_epoch = 20001;
         constexpr static bool enable_qsearch = false;
         constexpr static bool filter_in_check = false;
         constexpr static tune_t initial_learning_rate = 1;
-        constexpr static int32_t learning_rate_drop_interval = 1000;
+        constexpr static int32_t learning_rate_drop_interval = 2000;
         constexpr static tune_t learning_rate_drop_ratio = 0.5;
         constexpr static bool adam_bias_correction = true;
 
